3c4eb5a59551ae4355b0bae5bfa04d24b61d40af
sushilkaf <kaflesushilla56@gmail.com>
2025-05-23 11:03:40 +0545

    new changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import Sidebar from './Sidebar';
-import AppHeader from './AppHeader';
+
 import './Layout.css';
 
 export default function Layout({ children }) {
     return (
         <div className="layout">
-            <AppHeader />
+           
             <Sidebar />
             <main className="main-content">
                 {children}
